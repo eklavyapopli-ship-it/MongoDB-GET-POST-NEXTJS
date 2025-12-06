7c9dd0720049d0e2968c1b2685a9a32c5a28cb82
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-12-06 17:42:41 +0530

    get route added

---
+++ GET/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+import { Items } from "@/components/lib/model/review";
+
+export async function GET() {
+  
+try {
+    await mongoose.connect(process.env.MONGODB_URI!)
+     console.log("connected")
+  
+   const data = await  Items.find();
+
+     return NextResponse.json(data)
+      
+
+} catch (error) {
+    console.log(error)
+}
+
+   
+
+
+  
+}
